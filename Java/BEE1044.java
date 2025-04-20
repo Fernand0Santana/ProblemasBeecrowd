f4dbc6957a9f69b03b23a9ab56a182b0fd994815
José Luís Fernando do Carmo Santana <127857858+Fernand0Santana@users.noreply.github.com>
2025-04-20 13:33:12 -0300

    Create BEE1044.java

---
+++ Java/BEE1044.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        if(a>b) {
+            if(a%b==0) {
+                System.out.println("Sao Multiplos");
+            }
+            else {
+                System.out.println("Nao sao Multiplos");
+            }
+        }
+        else{
+            if(b%a==0) {
+                System.out.println("Sao Multiplos");
+            }
+            else {
+                System.out.println("Nao sao Multiplos");
+            }
+          
+        }
+    }
+}
